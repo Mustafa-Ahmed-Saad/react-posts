1ea02814561cb8c8c78105aec3e27a2818c00d08
osama7007 <osamasaadelsaeed@gmail.com>
2024-08-01 10:40:04 +0300

    get, post the post

--- front/posts-app/src/components/PostsList.jsx
+++ front/posts-app/src/components/PostsList.jsx
@@ -1,33 +1,89 @@
-import Post from "./Post";
-import classes from "./PostsList.module.css";
-import NewPost from "./NewPost";
-import Modal from "./Modal";
-import { useState } from "react";
+import Post from "./Post"
+import classes from "./PostsList.module.css"
+import NewPost from "./NewPost"
+import Modal from "./Modal"
+import { useEffect, useState } from "react"
 
 const PostsList = ({ isPosting, onStopPosting }) => {
-  const [posts, setPosts] = useState([]);
+    const [posts, setPosts] = useState([])
+    const [isFetching, setIsFetching] = useState(false)
 
-  function addPostHandler(post) {
-    setPosts((prevPosts) => [post, ...prevPosts]);
-  }
+    async function getPosts(postData) {
+        setIsFetching(true)
 
-  return (
-    <>
-      {isPosting && (
-        <Modal onClose={onStopPosting}>
-          <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
-        </Modal>
-      )}
-      {posts.length > 0 && (
-        <ul className={classes.posts}>
-          {posts.map((post, i) => (
-            <Post key={post.id} author={post.author} body={post.body} />
-          ))}
-        </ul>
-      )}
-      {posts.length === 0 && <p>No posts yet!</p>}
-    </>
-  );
-};
+        try {
+            const res = await fetch("http://localhost:8080/posts", {
+                method: "GET",
+                body: JSON.stringify(postData),
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            })
+            if (!res.ok) throw new Error("Failed to add post")
+            const data = await res.json()
+            setPosts(data.posts)
+            setIsFetching(false)
+            return data
+        } catch (e) {
+          setIsFetching(false)
+            console.log(e)
+        }
+    }
 
-export default PostsList;
+    async function addPostHandler(postData) {
+        try {
+            const res = await fetch("http://localhost:8080/posts", {
+                method: "POST",
+                body: JSON.stringify(postData),
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            })
+            if (!res.ok) throw new Error("Failed to add post")
+            const data = await res.json()
+            setPosts((prevPosts) => [postData, ...prevPosts])
+            return data
+        } catch (e) {
+            console.log(e)
+        }
+        // or
+        // const posts = await fetch("http://localhost:8080/posts", {
+        //     method: "POST",
+        //     body: JSON.stringify(postData),
+        //     headers: {
+        //         "Content-Type": "application/json",
+        //     },
+        // })
+        //     .then(async (res) => {
+        //         setPosts((prevPosts) => [postData, ...prevPosts])
+        //         const resJson = await res.json()
+        //         return resJson
+        //     })
+        //     .catch((e) => console.log(e))
+    }
+
+    useEffect(() => {
+        getPosts()
+    }, [])
+
+    return (
+        <>
+            {isPosting && (
+                <Modal onClose={onStopPosting}>
+                    <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
+                </Modal>
+            )}
+            {!isFetching && posts.length > 0 && (
+                <ul className={classes.posts}>
+                    {posts.map((post, i) => (
+                        <Post key={post.id} author={post.author} body={post.body} />
+                    ))}
+                </ul>
+            )}
+            {!isFetching && posts.length === 0 && <p>No posts yet!</p>}
+            {isFetching && <div>loading...</div>}
+        </>
+    )
+}
+
+export default PostsList
